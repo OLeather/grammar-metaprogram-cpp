2f0773b00878716fe8112e2b0f52a0f1eae6e860
Owen Leather <owenleather03@gmail.com>
2026-08-02 20:11:46 -0700

    parses the peg grammar, still some todos

--- examples/peg/peg.h
+++ examples/peg/peg.h
@@ -2,7 +2,174 @@
 
 #include "source/grammar.h"
 
+/*
+PEG defined in PEG
+Figure 1. of https://bford.info/pub/lang/peg.pdf
+
+# Hierarchical syntax
+Grammar <- Spacing Definition+ EndOfFile
+Definition <- Identifier LEFTARROW Expression
+
+Expression <- Sequence (SLASH Sequence)*
+Sequence <- Prefix*
+Prefix <- (AND / NOT)? Suffix
+Suffix <- Primary (QUESTION / STAR / PLUS)?
+Primary <- Identifier !LEFTARROW
+/ OPEN Expression CLOSE
+/ Literal / Class / DOT
+
+# Lexical syntax
+Identifier <- IdentStart IdentCont* Spacing
+IdentStart <- [a-zA-Z_]
+IdentCont <- IdentStart / [0-9]
+
+Literal <- [’] (![’] Char)* [’] Spacing
+/ ["] (!["] Char)* ["] Spacing
+
+Class <- ’[’ (!’]’ Range)* ’]’ Spacing
+Range <- Char ’-’ Char / Char
+Char <- ’\\’ [nrt’"\[\]\\]
+/ ’\\’ [0-2][0-7][0-7]
+/ ’\\’ [0-7][0-7]?
+/ !’\\’ .
+
+LEFTARROW <- ’<-’ Spacing
+SLASH <- ’/’ Spacing
+AND <- ’&’ Spacing
+NOT <- ’!’ Spacing
+QUESTION <- ’?’ Spacing
+STAR <- ’*’ Spacing
+PLUS <- ’+’ Spacing
+OPEN <- ’(’ Spacing
+CLOSE <- ’)’ Spacing
+DOT <- ’.’ Spacing
+
+Spacing <- (Space / Comment)*
+Comment <- ’#’ (!EndOfLine .)* EndOfLine
+Space <- ’ ’ / ’\t’ / EndOfLine
+EndOfLine <- ’\r\n’ / ’\n’ / ’\r’
+EndOfFile <- !.
+*/
+
 namespace peg {
 using namespace language;
+
+// clang-format off
+using EndOfLine = Or<Regex<R"(\r\n)">, Regex<R"(\n)">, Regex<R"(\r)">>;
+using Space = Or<Regex<" ">, Regex<R"(\t)">, EndOfLine>;
+using EndOfLineOrFile = Or<EndOfLine, EndOfFile>;
+using Comment = Sequence<Regex<R"(#)">, Repeated<Conditional<Not<EndOfLineOrFile>, Regex<R"(.)">>>>;  
+using Spacing = Repeated<Or<Space, Comment>>;
+
+using LEFTARROW = Sequence<Regex<R"(<-)">, Spacing>;
+using SLASH     = Sequence<Regex<R"(/)">, Spacing>;
+using AND       = Sequence<Regex<R"(&)">, Spacing>;
+using NOT       = Sequence<Regex<R"(!)">, Spacing>;
+using QUESTION  = Sequence<Regex<R"(\?)">, Spacing>;
+using STAR      = Sequence<Regex<R"(\*)">, Spacing>;
+using PLUS      = Sequence<Regex<R"(\+)">, Spacing>;
+using OPEN      = Sequence<Regex<R"(\()">, Spacing>;
+using CLOSE     = Sequence<Regex<R"(\))">, Spacing>;
+using DOT       = Sequence<Regex<R"(.)">, Spacing>;
+
+using Char = Or<
+  Regex<R"(\\[nrt'"\[\]\\])">,
+  Regex<R"(\\[0-2][0-7][0-7])">,
+  Regex<R"(\\[0-7][0-7]?)">,
+  Sequence<Regex<R"((?!\\))">, Regex<R"(.)">>
+>;
+
+using Range = Or<
+  Sequence<Char, Regex<R"(-)">, Char>,
+  Char
+>;
+
+using Class = Sequence<
+  Regex<R"(\[)">,
+  Repeated<Sequence<Regex<R"((?!\]))">, Range>>,
+  Regex<R"(\])">,
+  Spacing
+>;
+
+using Literal = Or<
+  Sequence<
+    Regex<R"(')">,
+    Repeated<Sequence<Regex<R"((?!'))">, Char>>,
+    Regex<R"(')">,
+    Spacing
+  >,
+  Sequence<
+    Regex<R"(")">,
+    Repeated<Sequence<Regex<R"((?!"))">, Char>>,
+    Regex<R"(")">,
+    Spacing
+  >
+>;
+
+
+using IdentStart = Regex<R"([a-zA-Z_])">;
+
+using IdentCont = Or<
+  IdentStart,
+  Regex<R"([0-9])">
+>;
+
+using Identifier = Sequence<
+  IdentStart,
+  Repeated<IdentCont>,
+  Spacing
+>;
+
+struct Expression;
+
+using Primary = Or<
+    Sequence<Identifier, Spacing, LEFTARROW, Spacing>,
+    Sequence<OPEN, Eval<Expression>, CLOSE>,
+    Literal,
+    Class,
+    DOT
+>;
+
+using Suffix = Or<
+  Sequence<
+    Primary,
+    Or<QUESTION, STAR, PLUS>>,
+  Primary
+>;
+
+using Prefix = Or<
+  Sequence<Or<AND, NOT>, Suffix>, 
+  Suffix
+>;
+
+using SequenceRule = Repeated<Prefix>;
+
+using ExpressionL = Sequence<
+    SequenceRule,
+    Repeated<Sequence<SLASH, SequenceRule>>
+>;
+
+
+struct Expression : ExpressionL {};
+
+using Definition = Sequence<
+    Identifier,
+    LEFTARROW,
+    Expression
+>;
+
+using Grammar = Sequence<
+    Spacing,
+    Repeated<Definition>,
+    Spacing,
+    EndOfFile
+>;
+
+// clang-format on
+
+/*
+Converters
+*/
+
 
 } // namespace peg
