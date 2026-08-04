6f9c57d3474f02674985cdfb21301336307380cf
Owen Leather <owenleather03@gmail.com>
2026-08-03 18:56:16 -0700

    cleanup

--- examples/peg/peg.h
+++ examples/peg/peg.h
@@ -55,15 +55,10 @@ namespace peg {
 using namespace language;
 
 // clang-format off
-
-/*
-Lexical Tokens & Operators
-*/
 using EndOfLine       = Or<Regex<R"(\r\n)">, Regex<R"(\n)">, Regex<R"(\r)">>;
 using Space           = Or<Regex<" ">, Regex<R"(\t)">, EndOfLine>;
 using EndOfLineOrFile = Or<EndOfLine, EndOfFile>;
 
-// Comment <- '#' (!EndOfLineOrFile .)* EndOfLineOrFile
 using Comment         = Sequence<Regex<R"(#)">, Repeated<Conditional<Not<EndOfLineOrFile>, Regex<R"(.)">>>, EndOfLineOrFile>;
 using Spacing         = Repeated<Or<Space, Comment>>;
 
@@ -78,102 +73,27 @@ using OPEN            = Sequence<Regex<R"(\()">, Spacing>;
 using CLOSE           = Sequence<Regex<R"(\))">, Spacing>;
 using DOT             = Sequence<Regex<R"(\.)">, Spacing>;
 
-/*
-Character Classes & Literals
-*/
-
-using Char = Or<
-  Regex<R"(\\[nrt'"\[\]\\])">,
-  Regex<R"(\\[0-2][0-7][0-7])">,
-  Regex<R"(\\[0-7][0-7]?)">,
-  Sequence<Not<Regex<R"(\\)">>, Regex<R"(.)">>
->;
-
-using Range = Or<
-  Sequence<Char, Regex<R"(-)">, Char>,
-  Char
->;
-
-using Class = Sequence<
-  Regex<R"(\[)">,
-  Repeated<Sequence<Not<Regex<R"(\])">>, Range>>,
-  Regex<R"(\])">,
-  Spacing
->;
-
+using Char    = Or<Regex<R"(\\[nrt'"\[\]\\])">, Regex<R"(\\[0-2][0-7][0-7])">, Regex<R"(\\[0-7][0-7]?)">, Sequence<Not<Regex<R"(\\)">>, Regex<R"(.)">>>;
+using Range   = Or<Sequence<Char, Regex<R"(-)">, Char>, Char>;
+using Class   = Sequence<Regex<R"(\[)">, Repeated<Sequence<Not<Regex<R"(\])">>, Range>>, Regex<R"(\])">, Spacing>;
 using Literal = Or<
-  Sequence<
-    Regex<R"(')">,
-    Repeated<Sequence<Not<Regex<R"(')">>, Char>>,
-    Regex<R"(')">,
-    Spacing
-  >,
-  Sequence<
-    Regex<R"(")">,
-    Repeated<Sequence<Not<Regex<R"(")">>, Char>>,
-    Regex<R"(")">,
-    Spacing
-  >
+  Sequence<Regex<R"(')">, Repeated<Sequence<Not<Regex<R"(')">>, Char>>, Regex<R"(')">, Spacing>,
+  Sequence<Regex<R"(")">, Repeated<Sequence<Not<Regex<R"(")">>, Char>>, Regex<R"(")">, Spacing>
 >;
 
-/*
-Identifiers
-*/
 using IdentStart = Regex<R"([a-zA-Z_])">;
 using IdentCont  = Or<IdentStart, Regex<R"([0-9])">>;
 using Identifier = Sequence<IdentStart, Repeated<IdentCont>, Spacing>;
 
-/*
-Hierarchical Grammar Rules
-*/
 struct Expression;
+using Primary = Or<Sequence<Identifier, Not<LEFTARROW>>, Sequence<OPEN, Eval<Expression>, CLOSE>, Literal, Class, DOT>;
+using Suffix  = Or<Sequence<Primary, Or<QUESTION, STAR, PLUS>>, Primary>;
+using Prefix  = Or<Sequence<Or<AND, NOT>, Suffix>,Suffix>;
 
-// Primary <- Identifier !LEFTARROW / OPEN Expression CLOSE / Literal / Class / DOT
-using Primary = Or<
-  Sequence<Identifier, Not<LEFTARROW>>,
-  Sequence<OPEN, Eval<Expression>, CLOSE>,
-  Literal,
-  Class,
-  DOT
->;
-
-// Suffix <- Primary (QUESTION / STAR / PLUS)?
-using Suffix = Or<
-  Sequence<Primary, Or<QUESTION, STAR, PLUS>>,
-  Primary
->;
-
-// Prefix <- (AND / NOT)? Suffix
-using Prefix = Or<
-  Sequence<Or<AND, NOT>, Suffix>,
-  Suffix
->;
-
-// Sequence <- Prefix*
 using SequenceRule = Repeated<Prefix>;
-
-// Expression <- Sequence (SLASH Sequence)*
-using ExpressionImpl = Sequence<
-  SequenceRule,
-  Repeated<Sequence<SLASH, SequenceRule>>
->;
-
-struct Expression : ExpressionImpl {};
-
-// Definition <- Identifier LEFTARROW Expression
-using Definition = Sequence<
-  Identifier,
-  LEFTARROW,
-  Expression
->;
-
-// Grammar <- Spacing Definition+ EndOfFile
-using Grammar = Sequence<
-  Spacing,
-  Repeated<Definition>,
-  EndOfFile
->;
-
+struct Expression  : Sequence<SequenceRule, Repeated<Sequence<SLASH, SequenceRule>>>{};
+using Definition   = Sequence<Identifier, LEFTARROW, Expression>;
+using Grammar      = Sequence<Spacing, Repeated<Definition>, EndOfFile>;
 // clang-format on
 
 } // namespace peg
