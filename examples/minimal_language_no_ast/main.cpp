040055f5493e1b66af7cf945bd2ea17b5f79ff34
Owen Leather <owenleather03@gmail.com>
2026-08-10 18:14:16 -0700

    working clean, no repeat

--- examples/minimal_language_no_ast/main.cpp
+++ examples/minimal_language_no_ast/main.cpp
@@ -12,13 +12,29 @@ using namespace example::syntax;
 //     }
 // };
 
-int main() {
-    std::string input = "((a))";
-    language::Context ctx{.input = input};
+struct Bindings {
+  void operator()(const Matcher<Number>::ReturnType &num) {
+    std::cout << "Number: " << num.match << std::endl;
+  }
 
-    if (auto result = Matcher<ExprGrammar>::Match(ctx, /*consume=*/true)) {
-        std::cout << "Parse success!\n";
-    } else {
-        std::cerr << "Parse failed!\n";
-    }
+  void operator()(const Matcher<Parenthesized>::TupleType &p) {
+    const auto expr_or_num_ref = std::get<1>(p);
+    const auto expr_or_num = expr_or_num_ref.get();
+    std::cout << "Parentesized: " << std::endl;
+    std::visit(Bindings{}, expr_or_num.value);
+  }
+};
+
+int main() {
+  std::string input = "((5))";
+  language::Context ctx{.input = input};
+
+  constexpr bool kConsume{true};
+  if (auto result = Matcher<Parenthesized>::Match(ctx, kConsume)) {
+    std::cout << "Parse success!\n";
+    Visitor{}(result->value);
+
+  } else {
+    std::cerr << "Parse failed!\n";
+  }
 }
--- examples/minimal_language_no_ast/syntax.h
+++ examples/minimal_language_no_ast/syntax.h
@@ -5,33 +5,23 @@ namespace example::syntax {
 
 using namespace language;
 
-// 1. Declare target struct
-struct Expr;
-
-// 2. Pure Grammar Definition
-using ExprGrammar =
-    grammar::Or<grammar::Regex<"a">,
-                grammar::Seq<grammar::Regex<"(">, grammar::Eval<Expr>,
-                             grammar::Regex<")">>>;
-
-// 3. Define target struct (NOW COMPLETE)
-struct Expr {
-  using Grammar = ExprGrammar;
-  using ReturnType = grammar::ReturnTypeOf<ExprGrammar>::type;
-  ReturnType value;
-};
-
-// struct Expr;
-
 // // using Whitespace = Repeated<Regex<"[ \\t\\r\\n]+">>;
-// using Number     = Regex<"[0-9]+">;
-// using Plus       = Regex<"\\+">;
-// using Star       = Regex<"\\*">;
-// using LParen     = Regex<"\\(">;
-// using RParen     = Regex<"\\)">;
+using Number = Regex<"[0-9]+">;
+using Plus = Regex<"\\+">;
+using Star = Regex<"\\*">;
+using LParen = Regex<"\\(">;
+using RParen = Regex<"\\)">;
 
-// // using Parenthesized = Seq<LParen, Plus, RParen>;
-// using Parenthesized = Seq<LParen, Eval<Expr>, RParen>;
+struct Expr;
+// using Parenthesized = Seq<LParen, Operator, RParen>;
+using Parenthesized = Seq<LParen, Eval<Expr>, RParen>;
+
+struct Expr : Or<Number, Parenthesized> {
+  using Grammar = Or<Number, Parenthesized>;
+  using ReturnType = ReturnTypeOf<Grammar>::type;
+  ReturnType value;
+  Expr(ReturnType t) : value(t){};
+};
 
 // struct Expr : Or<Number, Parenthesized>{};
 
