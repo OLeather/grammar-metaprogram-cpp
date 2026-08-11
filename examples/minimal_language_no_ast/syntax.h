65845452e4d513ffe153299399e8b1b9d3cca19d
Owen Leather <owenleather03@gmail.com>
2026-08-10 18:34:36 -0700

    cleanup

--- examples/minimal_language_no_ast/syntax.h
+++ examples/minimal_language_no_ast/syntax.h
@@ -6,37 +6,19 @@ namespace example::syntax {
 using namespace language;
 struct Expr;
 
-// // using Whitespace = Repeated<Regex<"[ \\t\\r\\n]+">>;
+using Whitespace = Repeated<Regex<"[ \\t\\r\\n]+">>;
 using Number = Regex<"[0-9]+">;
 using Plus = Regex<"\\+">;
 using Star = Regex<"\\*">;
 using LParen = Regex<"\\(">;
 using RParen = Regex<"\\)">;
 
-// using Parenthesized = Seq<LParen, Operator, RParen>;
-// using Parenthesized = Seq<LParen, Eval<Expr>, RParen>;
-
-// struct Expr : Or<Number, Parenthesized> {
-//   using Grammar = Or<Number, Parenthesized>;
-//   using ReturnType = ReturnTypeOf<Grammar>::type;
-//   ReturnType value;
-//   Expr(ReturnType t) : value(t){};
-// };
-
-// using Test = Repeated<Plus>;
-
-// struct Expr : Or<Number, Parenthesized>{};
-
 using Parenthesized = Seq<LParen, Eval<Expr>, RParen>;
-
-// // using Pluses = Repeated<Plus>;
-// // struct Expr : Or<Number, Parenthesized>{};
 
 using Factor = Or<Number, Parenthesized>;
 
 using Term = Seq<Factor, Repeated<Seq<Star, Factor>>>;
 
-// using ExprGrammar = Or<Seq<Term, Repeated<Seq<Plus, Term>>>, Term>;
 struct Expr : Or<Seq<Term, Repeated<Seq<Plus, Term>>>, Term> {
   using Grammar = Or<Seq<Term, Repeated<Seq<Plus, Term>>>, Term>;
   using ReturnType = ReturnTypeOf<Grammar>::type;
